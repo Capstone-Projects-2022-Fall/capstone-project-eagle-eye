a29262b025b9af380d8f781418970c0975d5e2e0
tyler3490 <61132056+tyler3490@users.noreply.github.com>
2022-09-28 17:09:59 -0400

    Update gui.py
    deleted some unneeded code, set the window title rather than using a header but left the header just in case

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ import sys
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import *
+import tkinter
 from tkinter.constants import LEFT, W
 from yolov5 import detect
  
@@ -12,21 +13,14 @@ class App():
 
     def __init__(self, width, height):
         # Tkinter root
-        self.root = tk.Tk()
-        # Define the width and height
+        self.root = tkinter.Tk()
+
+        # Define the width, height, and background color
         self.width = width
         self.height = height
- 
-        # define background color
         self.root.config(bg='#ffffff')
- 
-        # Tkinter objects
-        # Infile textbox object
-        self.tb_video_infile = None
 
-        # mode radio buttons
-        # self.rb_LIVE_mode = None
-        # self.rb_PRERECORDED_mode = None
+        # drop down options
         self.dd_mode_options = [
             "Live",
             "Prerecorded"
@@ -40,14 +34,13 @@ class App():
             "Ping Pong"
         ]
  
-        # Video input string -> For API
+        # Variables for the input file name, mode, and sport
         self.video_infile_name = StringVar()           
-        
-        # file type selected: 1 is live, 2 is prerecorded
-        # self.mode_checked = IntVar()
         self.mode_checked = StringVar()
-        self.mode_checked.set("Live")
         self.sport_checked = StringVar()
+
+        # set defaults for the dropdown menus 
+        self.mode_checked.set("Live")
         self.sport_checked.set("Tennis")
  
     def start(self):
@@ -59,23 +52,13 @@ class App():
         self.root.geometry(g)
  
         # Set window title
-        self.root.title('')
+        self.root.title('Eagle Eye')
  
         # Heading
-        heading = tk.Label(self.root, text = "Eagle Eye")
-        heading.config(font = ('Arial', 14), bg='#ffffff', fg='#000000')
+        # heading = tk.Label(self.root, text = "Eagle Eye")
+        # heading.config(font = ('Arial', 14), bg='#ffffff', fg='#000000')
  
-        # Infile
-        # video_infile_label = tk.Label(self.root, text = "Prerecorded Input File:", fg='#000000', font=('Arial', 12))                                  # Label
-        # self.tb_video_infile = tk.Entry(self.root, text = "Prerecorded Input File", width=40)                                      # Input Box
-        # video_infile_explore = tk.Button(self.root, text = "Browse", command = self.browse_infiles, fg='#000000', font=('Arial', 12))                          # Browse Button
-        # video_infile_label.config(bg='#ffffff')
-       
-        # radio buttons
-        # self.rb_LIVE_mode = Radiobutton(self.root, text="Live Mode", variable = self.mode_checked, value=1)
-        # self.rb_LIVE_mode.config(bg='#ffffff', fg='#000000', font=('Arial', 12))
-        # self.rb_PRERECORDED_mode = Radiobutton(self.root, text="Prerecorded Mode", variable = self.mode_checked, value=2)
-        # self.rb_PRERECORDED_mode.config(bg='#ffffff', fg='#000000', font=('Arial', 12))
+        # dropdown menus
         self.mode_dropdown = OptionMenu( self.root, self.mode_checked, *self.dd_mode_options)
         self.sport_dropdown = OptionMenu( self.root, self.sport_checked, *self.dd_sports_options)
  
@@ -85,23 +68,16 @@ class App():
         button_exit = tk.Button(self.root, text = "Exit", command = sys.exit, fg='#000000', font=('Arial', 12))
  
         # ---- Grid ---------------------------------------------------------------------------------------------------------------------------
-        heading.grid(row=0, column=2)
+        # heading.grid(row=0, column=2)
 
-        # add the label, textbox, and button for the input file 
-        # video_infile_label.grid(row=5, column=1)
-        # self.tb_video_infile.grid(row=5, column=2)
-        # video_infile_explore.grid (row = 5, column = 3)
-
-        # add a label for the mode
+        # add mode and sports selection dropdown and labels
         tk.Label(self.root, bg='#ffffff', text="Choose input type:", fg='#000000', font=('Arial', 12)).grid(row=7, column=1)
-        # self.rb_LIVE_mode.grid(row=7, column=2)
-        # self.rb_PRERECORDED_mode.grid(row=7, column=3)
         self.mode_dropdown.grid(row=7, column=2)
 
         tk.Label(self.root, bg='#ffffff', text="Choose Sport:", fg='#000000', font=('Arial', 12)).grid(row=8, column=1)
         self.sport_dropdown.grid(row=8, column=2)
  
-        # add the buttons
+        # add buttons
         button_enter.grid(row=16, column=2)
         button_exit.grid(row = 17, column = 2)
 
@@ -130,7 +106,6 @@ class App():
             else:
                 messagebox.showerror(title='Could not open video file', message="Could not open video file.")
                 self.root.mainloop()
-            
         else:
             # run script with live feed
             detect.run(source=0)
