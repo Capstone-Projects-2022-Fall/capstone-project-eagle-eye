9b6d39d4f2b085d09b5cc3bd327e9917b317b5da
tyler3490 <61132056+tyler3490@users.noreply.github.com>
2022-10-04 17:38:43 -0400

    Update unitTests.py
    updated test docs

--- unitTests.py
+++ unitTests.py
@@ -1,16 +1,14 @@
 import unittest
 import gui
 
-# testGui = gui.App()
-
-# testGui.__init__(290,200)
-# testGui.mode_checked = "Live"
-# testGui.enter()
-
-
 class TestGUI(unittest.TestCase):
-
+    """Tests all testable parts of the GUI
+    
+    Due to the nature of the tkinter program unit testing is limited. Anything with root.mainloop() puases execution and waits 
+    for input. Because of this we can only test functionality/functions without this behavior. 
+    """
     def test_mode(self):
+        """Tests the mode check variable"""
         testGui = gui.App(290,200)
         testGui.mode_checked.set("Live")
         self.assertEqual(testGui.mode_checked.get(), "Live")
@@ -18,6 +16,7 @@ class TestGUI(unittest.TestCase):
         self.assertEqual(testGui.mode_checked.get(), "Prerecorded")
         pass
     def test_sport(self):
+        """Tests the sport check variable"""
         testGui = gui.App(290,200)
         testGui.sport_checked.set("Tennis")
         self.assertEqual(testGui.sport_checked.get(), "Tennis")
@@ -33,11 +32,13 @@ class TestGUI(unittest.TestCase):
         self.assertEqual(testGui.sport_checked.get(), "Ping Pong")
         pass
     def test_cleanup(self):
+        """Tests the cleanup functionality"""
         testGui = gui.App(290,200)
         testGui.video_infile_name.set("test/file/name")
         self.assertEqual(testGui.video_infile_name.get(), "test/file/name")
         testGui.do_cleanup()
         self.assertEqual(testGui.video_infile_name.get(), "")
+        pass
 
 if __name__ == '__main__':
     unittest.main()
