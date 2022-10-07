4b6f81ecb61690a8f61176ead66bebf38f03bcd3
Robert Stachurski <you@example.com>
2022-10-07 17:21:58 -0400

    Added documentation for the sport abstract class

--- sport.py
+++ sport.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 class Sport(ABC):
+    """Abstract class for available sports and their models.
+    
+    Each sport will have a model and field property
+    """
     def __init__(self, sportname):
         self.sportname = sportname
 
@@ -9,17 +13,31 @@ class Sport(ABC):
     @property
     def field(self):
         return f"fieldof{self.sportname}.pt"
+        
     @abstractmethod
     def printname(self):
+        """For only test purpose. Will print the name of the sport"""
         pass
     
     @abstractmethod
     def setmodeloptions(model):
+        """Will change any needed options or arguments needed for the dectect script to run
+            Args:
+            model: the model that will be used
+            """
         pass
     @abstractmethod
     def setfieldoptions(model):
+        """Will change any needed options or arguments needed for the field of the sport
+            Args:
+            field: the field that will be used
+            """
         pass
 
     @abstractmethod
     def sendscript(model):
+        """Will send to the sport selector the updated script for dectect.py
+            Args:
+            model: the model that will be used
+            """
         pass
