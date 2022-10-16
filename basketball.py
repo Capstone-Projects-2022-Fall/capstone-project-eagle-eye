3d5cade2c8537083f36b0d497d585d48a67878ca
Athena Evans <70277494+APersonTakingClasses@users.noreply.github.com>
2022-10-16 19:47:53 -0400

    Update basketball.py

--- basketball.py
+++ basketball.py
@@ -10,10 +10,10 @@ class BasketBall(Sport):
         """ Get absolute path to resource, works for dev and for PyInstaller """
         relative_path = os.path.join('models', 'basketball', 'basketball.pt')
         base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-        print(os.path.join(base_path, relative_path))
+        # print(os.path.join(base_path, relative_path))
         return os.path.join(base_path, relative_path)
-        print(os.getcwd())
-        print(os.path.join(os.getcwd(), 'models', 'basketball', 'basketball.pt'))
+        # print(os.getcwd())
+        # print(os.path.join(os.getcwd(), 'models', 'basketball', 'basketball.pt'))
         return os.path.join(os.getcwd(), 'models', 'basketball', 'basketball.pt')
 
     def __init__(self, sportname):
