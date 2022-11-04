cdecd5d1a1eaa989b6a754a4432c3ff5f40173b6
tyler3490 <61132056+tyler3490@users.noreply.github.com>
2022-11-04 09:37:05 -0400

    still working

--- tennis.py
+++ tennis.py
@@ -6,6 +6,7 @@ import cv2
 from court_detector import CourtDetector
 import torch
 import math
+import numpy as np
 
 class Tennis(Sport):
     """Override superclass model with the path to actual sport model"""
@@ -76,19 +77,30 @@ class Tennis(Sport):
         court_detector = CourtDetector()
         frame=frame_in
         lines = court_detector.detect(frame)
-        for i in range(0, len(lines), 4):
-            x1, y1, x2, y2 = lines[i],lines[i+1], lines[i+2], lines[i+3]
-            cv2.line(frame, (int(x1),int(y1)),(int(x2),int(y2)), (0,0,255), 5)
-        #set the court refereence variable with the object with filled lines 
-        self.court_reference = court_detector
-        return frame
+        # If no court is found lines will be none 
+        if lines != None:
+            for i in range(0, len(lines), 4):
+                x1, y1, x2, y2 = lines[i],lines[i+1], lines[i+2], lines[i+3]
+                cv2.line(frame, (int(x1),int(y1)),(int(x2),int(y2)), (0,0,255), 5)
+            #set the court refereence variable with the object with filled lines 
+            self.court_reference = court_detector
+            return frame
+        else:
+            # trigger a function to have the user pick the court 
+            print("NO COURT DETECTED!!!")
+            return frame
     
     def trackCourt(self, frame_in):
-        lines = self.court_reference.track_court(frame_in)
-        for i in range(0, len(lines), 4):
-            x1, y1, x2, y2 = lines[i],lines[i+1], lines[i+2], lines[i+3]
-            cv2.line(frame_in, (int(x1),int(y1)),(int(x2),int(y2)), (0,0,255), 5) 
-        return frame_in
+        if self.court_reference != None:
+            lines = self.court_reference.track_court(frame_in)
+            for i in range(0, len(lines), 4):
+                x1, y1, x2, y2 = lines[i],lines[i+1], lines[i+2], lines[i+3]
+                cv2.line(frame_in, (int(x1),int(y1)),(int(x2),int(y2)), (0,0,255), 5) 
+            return frame_in
+        else:
+            # trigger a function to have the user pick the court 
+            print("NO COURT DETECTED!!!")
+            return frame_in
 
     def lineCall(self, xy_In, frame_in):
         if self.court_reference is not None:
@@ -120,6 +132,74 @@ class Tennis(Sport):
             self.prev_pos = ball_pos[:]
         # return the frame no matter what, if the condition wasnt met then its just the same frame that came in
         return frame_in
+
+    def warpCourt(self, frame_in):
+        ogFrame = frame_in
+        # bBaseline = self.court_reference.baseline_bottom
+        # tBaseline= self.court_reference.baseline_top
+        # Linner = self.court_reference.left_inner_line
+        # Rinner = self.court_reference.right_inner_line
+        # print(bBaseline)
+        # print(tBaseline)
+        # print(Linner)
+        # print(f"{int(Linner[0])}, {int(Linner[1])}")
+        # print(f"{Linner[2]}, {Linner[3]}")
+        # print(f"{Rinner[2]}, {Rinner[3]}")
+        # print(f"{Rinner[0]}, {Rinner[1]}")
+        # print(Rinner)
+        # pts_src = np.array([
+        #     [int(Linner[0]),int(Linner[1])],     # top left corner
+        #     [int(Linner[2]),int(Linner[3])],     # bottom left
+        #     [int(Rinner[2]),int(Rinner[3])],    # bottom right
+        #     [int(Rinner[0]),int(Rinner[1])],     # top right rorner
+        #     ])   
+
+        # 3secQatarTest.mp4 coordinates
+        # pts_src = np.array([
+        #     [642,212],     # top left corner
+        #     [392,712],     # bottom left
+        #     [1321,708],    # bottom right
+        #     [1069,210],     # top right rorner
+        #     ])   
+
+        # swingvisionmed1
+        # pts_src = np.array([
+        #     [816,291],     # top left corner
+        #     [124,867],     # bottom left
+        #     [1946,877],    # bottom right
+        #     [1235,293],     # top right rorner
+        #     ])   
+
+        # swingvisionmed2
+        pts_src = np.array([
+            [918,305],     # top left corner
+            [440,678],     # bottom left
+            [1668,718],    # bottom right
+            [1219,308],     # top right rorner
+            ]) 
+  
+        frame_in = cv2.polylines(frame_in, [pts_src], isClosed=True, color=[255,0,0], thickness=2)
+        # Read destination image.
+        img_dst = cv2.imread('/Users/tyler/Documents/GitHub/capstone-project-eagle-eye/court_configurations/court_reference.png')
+
+        # reference image points
+        pts_dst = np.array([
+            [421, 559],     # top left corner
+            [421, 2937],     # bottom left
+            [1244, 2937],    # bottom right
+            [1244, 559],     # top right corner
+            ])   
+
+        # Calculate Homography
+        h, status = cv2.findHomography(pts_src, pts_dst)
+        
+        # Warp source image to destination based on homography
+        img_src2 = ogFrame
+        img_out = cv2.warpPerspective(frame_in, h, (img_dst.shape[1], img_dst.shape[0]))
+        # cv2.imshow("Warped", img_out)
+        # cv2.waitKey(0)
+        # cv2.imshow("lines", frame_in)
+        return img_out
 
 # tennis = Tennis('tennis')
 
